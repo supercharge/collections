23af257a1c2d1d286f83fd5fabab88a2831bfbf3
Marcus Poehls <marcus.poehls@gmail.com>
2019-07-05 10:17:22 +0200

    export methods

---
+++ src/methods/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = {
+  every: require('./every'),
+  filter: require('./filter'),
+  find: require('./find'),
+  forEach: require('./forEach'),
+  map: require('./map'),
+  some: require('./some')
+}
