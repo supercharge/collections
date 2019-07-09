08ca7ad3383e03f626377dff92f685d6e1eadcb9
Marcus Pöhls <marcus.poehls@gmail.com>
2019-07-09 13:51:18 +0200

    renaming

--- src/collection.js
+++ src/collection.js
@@ -1,7 +1,83 @@
 'use strict'
 
-const Chainable = require('./chainable')
+const Methods = require('./methods')
+const Queue = require('@supercharge/queue-datastructure')
 
-module.exports = function collection (...args) {
-  return new Chainable(...args)
+class Collection {
+  constructor (array) {
+    this._callQueue = new Queue()
+    this._items = array
+  }
+
+  flatMap (callback) {
+    return this._enqueue('flatMap', callback)
+  }
+
+  map (callback) {
+    return this._enqueue('map', callback)
+  }
+
+  mapSeries (callback) {
+    return this._enqueue('mapSeries', callback)
+  }
+
+  filter (callback) {
+    return this._enqueue('filter', callback)
+  }
+
+  reduce (reducer, initial) {
+    return this.run(
+      this._enqueue('reduce', reducer, initial)
+    )
+  }
+
+  find (callback) {
+    return this.run(
+      this._enqueue('find', callback)
+    )
+  }
+
+  forEach (callback) {
+    return this.run(
+      this._enqueue('forEach', callback)
+    )
+  }
+
+  collapse (callback) {
+    return this._enqueue('collapse', callback)
+  }
+
+  every (callback) {
+    return this.run(
+      this._enqueue('every', callback)
+    )
+  }
+
+  some (callback) {
+    return this.run(
+      this._enqueue('some', callback)
+    )
+  }
+
+  _enqueue (method, callback, additional) {
+    this._callQueue.enqueue({ method: Methods[method], callback, additional })
+
+    return this
+  }
+
+  async run () {
+    while (this._callQueue.isNotEmpty()) {
+      try {
+        const { method, callback, additional } = this._callQueue.dequeue()
+        this._items = await method(this._items, callback, additional)
+      } catch (error) {
+        this._callQueue.clear()
+        throw error
+      }
+    }
+
+    return this._items
+  }
 }
+
+module.exports = Collection
