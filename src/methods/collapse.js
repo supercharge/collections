ae83e29cb02b7075d07354e6834ea3ba58cddc82
Marcus Pöhls <marcus.poehls@gmail.com>
2019-07-09 13:51:29 +0200

    add comment to collapse

--- src/methods/collapse.js
+++ src/methods/collapse.js
@@ -1,5 +1,12 @@
 'use strict'
 
+/**
+ * Collapse a collection of arrays into a single, flat collection.
+ *
+ * @param {Array} array
+ *
+ * @returns {Array}
+ */
 module.exports = function collapse (array) {
   return [].concat(...array)
 }
