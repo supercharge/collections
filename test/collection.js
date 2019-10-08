70106fc94d24d12a5e50f1011e2acb78fa1f3894
Ilya Ryabchinski <ilyaryabchinski@gmail.com>
2019-10-08 15:55:32 +0300

    add test with when two reverses provides original array

--- test/collection.js
+++ test/collection.js
@@ -672,8 +672,18 @@ describe('Chained Collection', () => {
     expect(
       await Collect([1, 2, 3, 2, 1]).reverse().all()
     ).to.equal([1, 2, 3, 2, 1])
+
+    const items = [1, 2, 3]
+    const collection = Collect(items)
+
+    expect(
+      await collection.reverse().all()
+    ).to.equal([3, 2, 1])
+    expect(
+      await collection.reverse().all()
+    ).to.equal([1, 2, 3])
   })
-    
+
   it('avg', async () => {
     expect(
       await Collect([1, 2, 3]).avg()
