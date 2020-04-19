bce0c7f0b34cba0f3ae7195ba2d4cec9001a7268
Marcus Poehls <marcus.poehls@gmail.com>
2020-04-19 14:54:05 +0200

    wip typescript

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 'use strict'
 
-const CollectionProxy = require('./src/collection-proxy')
-
-const collect = collection => {
-  return new CollectionProxy(collection)
-}
-
-module.exports = collect
-module.exports.default = collect
+module.exports = require('./dist')
